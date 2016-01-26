02488da15d6ffa666711a37543b620cfde6f246e
Roostersky <michalski.mario@gmail.com>
2016-01-26 14:28:15 +0000

    Update ArrayReverse.java
    Replacing BufferedReader and InputStreamReader with Scanner, used Arrays features to reverse array.

--- sample/src/sample/ArrayReverse.java
+++ sample/src/sample/ArrayReverse.java
@@ -6,7 +6,29 @@ import java.io.*;
 public class ArrayReverse {
 	
 	public static void main(String[] args) throws IOException{
+		
+		//Can use Scanner rather than BufferedReader and InputStreamReader - ease of use in this case
+		Scanner input = new Scanner(System.in);
+		
+		int[] num = new int[10];
+		
+		//Populating array
+		for (int i = 0; i < num.length; i++){
+			System.out.println("Enter number (integer): ");
+			num[i] = input.nextInt(); //nextInt - checks if entered number is Integer or int already
+		}
+		
+		input.close(); //closing stream
+		
+		//Use Arrays class features to reverse an array
+		int[] copyNum = num.clone(); //Creating copy of num array - don't want to modify original
+		Arrays.reverse(copyNum); //Reversing copy of num
+		
+		for (int i : copyNum){
+			System.out.println(i + " ");
+		}
 	
+	/**
 	InputStreamReader inStream = new InputStreamReader(System.in);
 	BufferedReader stdin = new BufferedReader(inStream);
 	
@@ -32,7 +54,7 @@ public class ArrayReverse {
 	
 	System.out.println( num[9]+ " "+ num[8]+ " " +num[7]+ " " +num[6]+ " "+num[5]+ " "+num[4]+ " "+num[3]+ " "+num[2]+ " "+num[1]+ " "+num[0]+ " ");
 	
-	
+	*//
 	
 		
 	
