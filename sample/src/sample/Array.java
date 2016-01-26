12ecb244a0ff2cd61601d09fd0c594934359aef8
Roostersky <michalski.mario@gmail.com>
2016-01-26 13:14:10 +0000

    Update Array.java
    Added static method to Array class that simplifies printing the result to the screen ;) Just for fun!

--- sample/src/sample/Array.java
+++ sample/src/sample/Array.java
@@ -1,10 +1,24 @@
 package sample;
 
 public class Array {
+	
+	//Prints values stored in array to the screen
+	public static void printArray(int[] anArray){
+		for (int i : anArray){
+			System.out.println("Value is: " + i + "\n");
+			System.out.println("Testing Repository");
+			System.out.println("Added new line on test repo 01/21/16");
+		}
+	}
 
 	public static void main(String[] args) {
-	
+		
+		//Declared new variable and assignment of values.
 		int[] ramesh = {1, 2, 3, 4, 5};
+		
+		//Will produce the same result as the method below.
+		Array.printArray(ramesh);
+		
 		
 		for (int i = 0; i < ramesh.length; i ++) {
 			
